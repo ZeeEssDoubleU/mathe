9bf1b78d26d254693199d138aee71022540bf1e3
Zak Williams <zak.williams2287@gmail.com>
2020-09-23 21:07:38 -0400

    fix:  adjusting SEO imports

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,138 +1,128 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import { useStaticQuery, graphql } from "gatsby";
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
 // ************
 // component
 // ************
 
-const SEO = (props) => {
-	const { site, datoCmsSite } = useStaticQuery(query);
-	const { siteMetadata } = site;
-	const { globalSeo } = datoCmsSite;
+export default function SEO(props) {
+  const { site, datoCmsSite } = useStaticQuery(query)
+  const { siteMetadata } = site
+  const { globalSeo } = datoCmsSite
 
-	return (
-		<Helmet
-			title={props.title || siteMetadata.title || globalSeo.siteName}
-			titleTemplate={
-				"%s" + siteMetadata.titleTemplate || "%s" + globalSeo.titleSuffix
-			}
-		>
-			<html lang={siteMetadata.lang} />
+  return (
+    <Helmet
+      title={props.title || siteMetadata.title || globalSeo.siteName}
+      titleTemplate={
+        "%s" + siteMetadata.titleTemplate || "%s" + globalSeo.titleSuffix
+      }
+    >
+      <html lang={siteMetadata.lang} />
 
-			{/* serp */}
-			<meta
-				name="description"
-				content={
-					props.description ||
-					siteMetadata.description ||
-					globalSeo.fallbackSeo.description
-				}
-			/>
-			<meta
-				name="image"
-				content={
-					props.image ||
-					siteMetadata.image ||
-					globalSeo.fallbackSeo.image.url
-				}
-			/>
-			<meta name="theme-color" content={siteMetadata.themeColor} />
-			<meta
-				name="keywords"
-				content={props.keywords || siteMetadata.keywords}
-			/>
+      {/* serp */}
+      <meta
+        name="description"
+        content={
+          props.description ||
+          siteMetadata.description ||
+          globalSeo.fallbackSeo.description
+        }
+      />
+      <meta
+        name="image"
+        content={
+          props.image || siteMetadata.image || globalSeo.fallbackSeo.image.url
+        }
+      />
+      <meta name="theme-color" content={siteMetadata.themeColor} />
+      <meta name="keywords" content={props.keywords || siteMetadata.keywords} />
 
-			{/* facebook */}
-			<meta property="og:url" content={props.url || siteMetadata.siteUrl} />
-			<meta
-				property="og:title"
-				content={
-					props.title || siteMetadata.title || globalSeo.fallbackSeo.title
-				}
-			/>
-			<meta
-				property="og:description"
-				content={
-					props.description ||
-					siteMetadata.description ||
-					globalSeo.fallbackSeo.description
-				}
-			/>
-			<meta
-				property="og:image"
-				content={
-					props.image ||
-					siteMetadata.image ||
-					globalSeo.fallbackSeo.image.url
-				}
-			/>
+      {/* facebook */}
+      <meta property="og:url" content={props.url || siteMetadata.siteUrl} />
+      <meta
+        property="og:title"
+        content={
+          props.title || siteMetadata.title || globalSeo.fallbackSeo.title
+        }
+      />
+      <meta
+        property="og:description"
+        content={
+          props.description ||
+          siteMetadata.description ||
+          globalSeo.fallbackSeo.description
+        }
+      />
+      <meta
+        property="og:image"
+        content={
+          props.image || siteMetadata.image || globalSeo.fallbackSeo.image.url
+        }
+      />
 
-			{/* twitter */}
-			<meta name="twitter:card" content="summary" />
-			<meta
-				name="twitter:title"
-				content={
-					props.title || siteMetadata.title || globalSeo.fallbackSeo.title
-				}
-			/>
-			<meta
-				name="twitter:description"
-				content={
-					props.description ||
-					siteMetadata.description ||
-					globalSeo.fallbackSeo.description
-				}
-			/>
-			<meta
-				name="twitter:image"
-				content={
-					props.image ||
-					siteMetadata.image ||
-					globalSeo.fallbackSeo.image.url
-				}
-			/>
-		</Helmet>
-	);
-};
-export default SEO;
+      {/* twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta
+        name="twitter:title"
+        content={
+          props.title || siteMetadata.title || globalSeo.fallbackSeo.title
+        }
+      />
+      <meta
+        name="twitter:description"
+        content={
+          props.description ||
+          siteMetadata.description ||
+          globalSeo.fallbackSeo.description
+        }
+      />
+      <meta
+        name="twitter:image"
+        content={
+          props.image || siteMetadata.image || globalSeo.fallbackSeo.image.url
+        }
+      />
+    </Helmet>
+  )
+}
 
 // ************
 // query
 // ************
 
 const query = graphql`
-	{
-		site {
-			siteMetadata {
-				title
-				titleTemplate
-				description
-				siteUrl
-				image
-				keywords
-				themeColor
-				lang
-			}
-		}
-		datoCmsSite {
-			faviconMetaTags {
-				...GatsbyDatoCmsFaviconMetaTags
-			}
-			globalSeo {
-				facebookPageUrl
-				fallbackSeo {
-					title
-					twitterCard
-					description
-					image {
-						url
-					}
-				}
-				siteName
-				twitterAccount
-				titleSuffix
-			}
-		}
-	}
-`;
+  {
+    site {
+      siteMetadata {
+        title
+        titleTemplate
+        description
+        siteUrl
+        image
+        keywords
+        themeColor
+        lang
+      }
+    }
+    datoCmsSite {
+      faviconMetaTags {
+        ...GatsbyDatoCmsFaviconMetaTags
+      }
+      globalSeo {
+        facebookPageUrl
+        fallbackSeo {
+          title
+          twitterCard
+          description
+          image {
+            url
+          }
+        }
+        siteName
+        twitterAccount
+        titleSuffix
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
 // import components
-import SEO from "../components/SEO";
+import SEO from "../components/SEO"
 
 // ************
 // component
 // ************
 
 const App = () => {
-	// let the document know when mouse is being used
-	useEffect(() => {
-		document.body.addEventListener("mousedown", () => {
-			document.body.classList.add("using-mouse");
-		});
-		document.body.addEventListener("keydown", () => {
-			document.body.classList.remove("using-mouse");
-		});
-	}, []);
+  // let the document know when mouse is being used
+  useEffect(() => {
+    document.body.addEventListener("mousedown", () => {
+      document.body.classList.add("using-mouse")
+    })
+    document.body.addEventListener("keydown", () => {
+      document.body.classList.remove("using-mouse")
+    })
+  }, [])
 
-	return <SEO />;
-};
-export default App;
+  return <SEO />
+}
+export default App
