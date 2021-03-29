f59111c026f255cfcf71abdb1fe108751fe593b5
Zak Williams <zak.williams2287@gmail.com>
2021-03-28 23:45:51 -0400

    contact-form:  corrected ajax encoding

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,4 +1,10 @@
-import React, { useState, ReactElement, FormEvent, ChangeEvent } from "react"
+import React, {
+	useState,
+	ReactElement,
+	FormEvent,
+	ChangeEvent,
+	useRef,
+} from "react"
 import styled from "styled-components"
 import { navigate } from "gatsby"
 import axios from "axios"
@@ -10,8 +16,9 @@ import Icon from "../Icons/Icon"
 // ************
 
 export default function ContactForm(): ReactElement {
+	const formRef = useRef(null)
 	const [formData, setFormData] = useState({
-		"form-name": "contact-form",
+		"honeypot-field": "",
 		name: "",
 		email: "",
 		subject: "",
@@ -23,9 +30,9 @@ export default function ContactForm(): ReactElement {
 		// use entries for better typing
 		return Object.entries(data)
 			.map((entry) => {
-				const key = entry[0]
-				const value = entry[1]
-				return encodeURIComponent(`${key}=${value}`)
+				const key_encoded = encodeURIComponent(entry[0])
+				const value_encoded = encodeURIComponent(entry[1])
+				return `${key_encoded}=${value_encoded}`
 			})
 			.join("&")
 	}
@@ -33,13 +40,17 @@ export default function ContactForm(): ReactElement {
 	// handle form submit
 	async function handleSubmit(event: FormEvent) {
 		event.preventDefault()
+		const form = useRef.current
 
 		try {
 			const response = await axios({
-				url: `/.netlify/functions/form`,
+				url: "/.netlify/functions/form",
 				method: "POST",
 				headers: { "Content-Type": "application/x-www-form-urlencoded" },
-				data: encode(formData),
+				data: encode({
+					"form-name": form.getAttribute("name"),
+					...formData,
+				}),
 			})
 			console.log(response.data) // ? debug
 
@@ -63,6 +74,7 @@ export default function ContactForm(): ReactElement {
 
 	return (
 		<Form
+			ref={formRef}
 			id="contact-form"
 			name="contact-form"
 			method="POST"
@@ -74,7 +86,12 @@ export default function ContactForm(): ReactElement {
 			{/* input required by netlify for SSGs like gatsby */}
 			<input type="hidden" name="form-name" value="contact-form" />
 			{/* hidden honeypot field meant to capture bots */}
-			<input name="honeypot-field" style={{ display: "none" }} />
+			<input
+				name="honeypot-field"
+				style={{ display: "none" }}
+				value={formData["honeypot-field"]}
+				onChange={handleChange}
+			/>
 			<input
 				required
 				type="text"
@@ -113,7 +130,8 @@ export default function ContactForm(): ReactElement {
 				value={formData["message"]}
 				onChange={handleChange}
 			/>
-			<button type="submit" aria-label="submit form">
+
+			<button aria-label="submit form">
 				<Icon name="send" />
 			</button>
 		</Form>
@@ -125,64 +143,65 @@ export default function ContactForm(): ReactElement {
 // ************
 
 const Form = styled.form`
-	display: flex;
-	flex-wrap: wrap;
-	& > * {
-		margin: 6px 8px;
-	}
-	.contact-form-input {
-		flex-grow: 1;
-		background: none;
-		padding: 1rem;
-		border: 1px solid white;
-		border-radius: 04px;
-		color: white;
-		/* TODO: consider adding focus animation */
-		/* transition: transform 0.2s, box-shadow 0.2s;
-		&:required {
-			box-shadow: none;
-		}
-		&:focus {
-			transform: scale(1.02);
-			box-shadow: 0 0 0 1px
-				hsla(${({ theme }) => theme.color.app_green_partial}, 0.5);
-			&::placeholder {
-				opacity: 0;
-			}
-		} */
-	}
-	.contact-form-response {
-		width: 100%;
-		text-align: center;
-	}
-	textarea {
-		width: 100%;
-	}
-	button {
-		display: grid;
-		align-content: center;
-		justify-content: center;
+   display: flex;
+   flex-wrap: wrap;
+   & > * {
+      margin: 6px 8px;
+   }
+   .contact-form-input {
+      flex-grow: 1;
+      background: none;
+      padding: 1rem;
+      border: 1px solid white;
+      border-radius: 04px;
+      color: white;
+      /* TODO: consider adding focus animation */
+      /* transition: transform 0.2s, box-shadow 0.2s;
+      &:required {
+         box-shadow: none;
+      }
+      &:focus {
+         transform: scale(1.02);
+         box-shadow: 0 0 0 1px
+            hsla(${({ theme }) => theme.color.app_green_partial}, 0.5);
+         &::placeholder {
+            opacity: 0;
+         }
+      } */
+   }
+   .contact-form-response {
+      width: 100%;
+      text-align: center;
+   }
+   textarea {
+      width: 100%;
+   }
+   button {
+      color: white,
+      display: grid;
+      align-content: center;
+      justify-content: center;
 
-		width: 64px;
-		height: 64px;
-		border: 1px solid ${({ theme }) => theme.color.app_green};
-		margin: 16px auto;
+      width: 64px;
+      height: 64px;
+      border: 1px solid ${({ theme }) => theme.color.app_green};
+      margin: 16px auto;
 
-		background: none;
-		border-radius: 50%;
-		transition: background 200ms;
-		svg {
-			height: 30px;
-			width: 30px;
-			margin: 0 auto;
-			fill: white;
-		}
-		&:hover {
-			cursor: pointer;
-			background: hsla(${({ theme }) => theme.color.app_green_partial}, 0.5);
-			svg {
-				fill: white;
-			}
-		}
-	}
+      background: none;
+      border-radius: 50%;
+      transition: background 200ms;
+      svg {
+         height: 30px;
+         width: 30px;
+         margin: 0 auto;
+         fill: white;
+      }
+      &:hover {
+         cursor: pointer;
+         background: hsla(${({ theme }) => theme.color.app_green_partial}, 0.5);
+         svg {
+            fill: white;
+         }
+      }
+   }
 `
