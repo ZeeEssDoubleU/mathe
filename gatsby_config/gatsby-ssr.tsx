73c28a70cf102c6222d58f2aca8171f29871d659
Zak Williams <zak.williams2287@gmail.com>
2021-04-09 15:03:19 -0400

    ssr: - added preconnects

--- gatsby_config/gatsby-ssr.tsx
+++ gatsby_config/gatsby-ssr.tsx
@@ -1,10 +1,24 @@
 import { RenderBodyArgs } from "gatsby"
+import React from "react"
 import WrapRoot from "../src/components/GatsbyAPI/WrapRoot"
 
 // root element
 export const wrapRootElement = WrapRoot
 
-// // render body
-// export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs): void => {
-// 	setHeadComponents(headComponents)
-// }
+// render body
+export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs): void => {
+	const headComponents = [
+		<link
+			key="google_font_preconnect"
+			rel="preconnect"
+			href="https://fonts.googleapis.com"
+		/>,
+		<link
+			key="datocms_preconnect"
+			rel="preconnect"
+			href="https://www.datocms-assets.com"
+		/>,
+	]
+
+	setHeadComponents(headComponents)
+}
